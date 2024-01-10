73cc2c46265be1bb983a139f87f6ac2d2ae8b38f
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-01-10 11:44:33 -0500

    feat (Architecture): adds the common IHaveComponents interface
    This is to facilitate a future update where pulling out nested components is possible.

---
+++ src/Rhythm.Drop.Models/Common/IHaveComponents.cs
@@ -0,0 +1,16 @@
+﻿namespace Rhythm.Drop.Models.Common;
+
+using Rhythm.Drop.Models.Components;
+using System.Collections.Generic;
+
+/// <summary>
+/// A contract for creating an component that has components within it.
+/// </summary>
+public interface IHaveComponents
+{
+    /// <summary>
+    /// Gets the components within this object.
+    /// </summary>
+    /// <returns>A <see cref="IReadOnlyCollection{T}"/> of <see cref="IComponent"/>.</returns>
+    IReadOnlyCollection<IComponent> GetComponents();
+}
