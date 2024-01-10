70be3c9731b0b21e766ab99e24aa2dc3c7a47479
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-01-10 15:47:59 -0500

    chore (Startup): renames RhythmBuilderExtensions to ServiceCollectionExtensions
    This better implies the intent of the code.

--- src/Rhythm.Drop/ServiceCollectionExtensions.cs
+++ src/Rhythm.Drop/ServiceCollectionExtensions.cs
@@ -7,14 +7,14 @@ using Rhythm.Drop.Web;
 /// <summary>
 /// A collection of extension methods that augment implementations of <see cref="IServiceCollection"/> regarding Rhythm Drop.
 /// </summary>
-public static class RhythmBuilderExtensions
+public static class ServiceCollectionExtensions
 {
     /// <summary>
     /// Adds Rhythm Drop with default dependencies to the current <see cref="IServiceCollection"/>.
     /// </summary>
     /// <param name="services">The current services.</param>
     /// <returns>A <see cref="IServiceCollection"/>.</returns>
-    /// <remarks>For a more controlled installation use <see cref="ServiceCollectionExtensions.AddRhythmDrop(IServiceCollection, Action{IRhythmDropBuilder})"/></remarks>
+    /// <remarks>For a more controlled installation use <see cref="Infrastructure.ServiceCollectionExtensions.AddRhythmDrop(IServiceCollection, Action{IRhythmDropBuilder})"/></remarks>
     public static IServiceCollection AddRhythmDrop(this IServiceCollection services)
     {
         return services.AddRhythmDrop((builder) =>
