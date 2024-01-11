6686d85eef8591c73d02c5f4f30ef992dd16356b
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-01-11 11:40:41 -0500

    feat (Links): moves modal link persistence to tag helper from renderer
    Persisting the modal shouldn't be a concern of the renderer. This also takes some weight off custom implementations of IDropLinkTagHelperRenderer.

--- src/Rhythm.Drop.Web/TagHelperRenderers/Links/DefaultDropLinkTagHelperRenderer.cs
+++ src/Rhythm.Drop.Web/TagHelperRenderers/Links/DefaultDropLinkTagHelperRenderer.cs
@@ -2,22 +2,15 @@
 
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Rhythm.Drop.Models.Links;
-using Rhythm.Drop.Web.Infrastructure.Helpers.Modals;
 using Rhythm.Drop.Web.Infrastructure.TagHelperRenderers.Links;
 using System.Threading.Tasks;
 
 /// <summary>
 /// The default implementation of <see cref="IDropLinkTagHelperRenderer"/>.
 /// </summary>
-/// <param name="modalPersistenceHelper">The modal persistence helper.</param>
 /// <remarks>This implementation should cover most scenarios but can be replaced if needed on a project-by-project basis.</remarks>
-internal sealed class DefaultDropLinkTagHelperRenderer(IModalPersistenceHelper modalPersistenceHelper) : DropLinkTagHelperRendererBase
+internal sealed class DefaultDropLinkTagHelperRenderer() : DropLinkTagHelperRendererBase
 {
-    /// <summary>
-    /// The modal persistence helper.
-    /// </summary>
-    private readonly IModalPersistenceHelper _modalPersistenceHelper = modalPersistenceHelper;
-
     /// <inheritdoc/>
     protected override async Task RenderModelAsync(ILink link, TagHelperContext context, TagHelperOutput output)
     {
@@ -25,7 +18,6 @@ internal sealed class DefaultDropLinkTagHelperRenderer(IModalPersistenceHelper m
         {
             if (link is IModalLink modalLink)
             {
-                _modalPersistenceHelper.Persist(modalLink);
                 output.Attributes.SetAttribute("data-modal-target", modalLink.Modal.UniqueKey);
             }
 
--- src/Rhythm.Drop.Web/TagHelpers/DropLinkTagHelper.cs
+++ src/Rhythm.Drop.Web/TagHelpers/DropLinkTagHelper.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Rhythm.Drop.Models.Links;
+using Rhythm.Drop.Web.Infrastructure.Helpers.Modals;
 using Rhythm.Drop.Web.Infrastructure.TagHelperRenderers.Links;
 using System.Threading.Tasks;
 
@@ -9,13 +10,19 @@ using System.Threading.Tasks;
 /// A tag helper that renders a <see cref="ILink"/>.
 /// </summary>
 /// <param name="tagHelperRenderer">The tag helper renderer.</param>
+/// <param name="modalPersistenceHelper">The modal persistence helper.</param>
 [HtmlTargetElement("drop-link", TagStructure = TagStructure.NormalOrSelfClosing)]
-public sealed class DropLinkTagHelper(IDropLinkTagHelperRenderer tagHelperRenderer) : TagHelper
+public sealed class DropLinkTagHelper(IDropLinkTagHelperRenderer tagHelperRenderer, IModalPersistenceHelper modalPersistenceHelper) : TagHelper
 {
     /// <summary>
     /// The tag helper renderer.
     /// </summary>
     private readonly IDropLinkTagHelperRenderer _tagHelperRenderer = tagHelperRenderer;
+
+    /// <summary>
+    /// The modal persistence helper.
+    /// </summary>
+    private readonly IModalPersistenceHelper _modalPersistenceHelper = modalPersistenceHelper;
 
     /// <summary>
     /// Gets or sets the <see cref="ILink"/> model.
@@ -26,6 +33,11 @@ public sealed class DropLinkTagHelper(IDropLinkTagHelperRenderer tagHelperRender
     /// <inheritdoc/>
     public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
     {
+        if (Model is IModalLink modalLink)
+        {
+            _modalPersistenceHelper.Persist(modalLink);
+        }
+
         await _tagHelperRenderer.RenderAsync(Model, context, output);
     }
 
