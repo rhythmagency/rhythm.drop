265ae63b25e6fde6764e015f061cde243333a1bc
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-01-12 13:15:05 -0500

    feat (Components): removes render component view component
    This no longer required by the default components tag helper renderer.

--- src/Rhythm.Drop.Web/ViewComponents/RenderComponentViewComponentInput.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Rhythm.Drop.Web.ViewComponents;
-
-/// <summary>
-/// The input required to render a component view component.
-/// </summary>
-/// <param name="Model">The component or component meta data.</param>
-/// <param name="ViewPath">The view path to render the component with.</param>
-public sealed record RenderComponentViewComponentInput(object Model, string ViewPath);
