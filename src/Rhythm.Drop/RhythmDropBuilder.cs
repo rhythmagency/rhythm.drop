1cb27c206d4b27511dc938eb9e8dcbbf54106c55
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-01-15 14:37:16 -0500

    chore (DI): moves implementation of RhythmDropBuilder to main project
    This didn't add any value in infrastructure and is only ever used in the main project.

---
+++ src/Rhythm.Drop/RhythmDropBuilder.cs
@@ -0,0 +1,16 @@
+﻿namespace Rhythm.Drop;
+
+using Rhythm.Drop.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// An implementation of <see cref="IRhythmDropBuilder"/>.
+/// </summary>
+/// <param name="services">
+/// The services.
+/// </param>
+public sealed class RhythmDropBuilder(IServiceCollection services) : IRhythmDropBuilder
+{
+    /// <inheritdoc/>
+    public IServiceCollection Services => services;
+}
