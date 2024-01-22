9877f6fdae5aece81e2861f589c3c148fa9774c9
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-01-22 13:41:05 -0500

    feat (Project): adds basic NUnit test project for Builders

---
+++ tests/Rhythm.Drop.Builders.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Rhythm.Drop.Builders.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
