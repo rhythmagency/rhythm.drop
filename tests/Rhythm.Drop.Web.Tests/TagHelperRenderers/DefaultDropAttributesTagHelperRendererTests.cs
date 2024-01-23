6e62ca71329277afb03897dfd7425a6bfbfe4fb9
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-01-23 11:16:01 -0500

    feat (Tests): tests for drop attribute tag helper renderer

---
+++ tests/Rhythm.Drop.Web.Tests/TagHelperRenderers/DefaultDropAttributesTagHelperRendererTests.cs
@@ -0,0 +1,94 @@
+﻿namespace Rhythm.Drop.Web.Tests.TagHelperRenderers;
+
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Rhythm.Drop.Models.Common.Attributes;
+using Rhythm.Drop.Web.TagHelperRenderers.Attributes;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+[TestFixture]
+public class DefaultDropAttributesTagHelperRendererTests : TagHelperRendererTestsBase
+{
+    private const string TestAttributeName = "data-test";
+
+    [Test]
+    public async Task RenderAsync_With_Attributes_Should_Return_Attributes()
+    {
+        // arrange
+        var context = CreateTagHelperContext(DefaultTagName);
+        var output = CreateTagHelperOutput(DefaultTagName);
+        var tagHelpererRenderer = new DefaultDropAttributesTagHelperRenderer();
+
+        var attributes = new HtmlAttributeCollection();
+        attributes.SetAttribute(TestAttributeName, true);
+
+        // act
+        await tagHelpererRenderer.RenderAsync(attributes, context, output);
+
+        Assert.That(output.Attributes, Has.Count.EqualTo(attributes.Count));
+    }
+
+    [Test]
+    public async Task RenderAsync_With_Attributes_Should_Return_Additional_Attributes()
+    {
+        // arrange
+        var existingAttributes = new List<TagHelperAttribute>()
+        {
+            new(TestAttributeName, true)
+        };
+        var context = CreateTagHelperContext(DefaultTagName, existingAttributes);
+        var output = CreateTagHelperOutput(DefaultTagName, existingAttributes);
+        var tagHelpererRenderer = new DefaultDropAttributesTagHelperRenderer();
+
+        var attributes = new HtmlAttributeCollection();
+        attributes.SetAttribute("data-new", true);
+
+        // act
+        await tagHelpererRenderer.RenderAsync(attributes, context, output);
+
+        Assert.That(output.Attributes, Has.Count.EqualTo(attributes.Count + existingAttributes.Count));
+    }
+
+    [Test]
+    public async Task RenderAsync_With_Attributes_Should_Return_Overridden_Attributes()
+    {
+        // arrange
+        const bool OldValue = true;
+        const bool NewValue = true;
+        var existingAttributes = new List<TagHelperAttribute>()
+        {
+            new(TestAttributeName, OldValue)
+        };
+        var context = CreateTagHelperContext(DefaultTagName, existingAttributes);
+        var output = CreateTagHelperOutput(DefaultTagName, existingAttributes);
+        var tagHelpererRenderer = new DefaultDropAttributesTagHelperRenderer();
+
+        var attributes = new HtmlAttributeCollection();
+        attributes.SetAttribute(TestAttributeName, NewValue);
+
+        // act
+        await tagHelpererRenderer.RenderAsync(attributes, context, output);
+
+        Assert.That(output.Attributes, Has.Count.EqualTo(attributes.Count));
+        Assert.That(output.Attributes, Has.Exactly(1).Matches<TagHelperAttribute>(x => x.Name == TestAttributeName && x.Value.ToString() == NewValue.ToString()));
+    }
+
+    [Test]
+    public async Task RenderAsync_With_No_Attributes_Should_Return_Existing_Attributes()
+    {
+        // arrange
+        var existingAttributes = new List<TagHelperAttribute>()
+        {
+            new(TestAttributeName, true)
+        };
+        var context = CreateTagHelperContext(DefaultTagName, existingAttributes);
+        var output = CreateTagHelperOutput(DefaultTagName, existingAttributes);
+        var tagHelpererRenderer = new DefaultDropAttributesTagHelperRenderer();
+        HtmlAttributeCollection? attributes = default;
+
+        // act
+        await tagHelpererRenderer.RenderAsync(attributes, context, output);
+
+        Assert.That(output.Attributes, Has.Count.EqualTo(existingAttributes.Count));
+    }
+}
---
+++ tests/Rhythm.Drop.Web.Tests/TagHelperRenderers/TagHelperRendererTestsBase.cs
@@ -0,0 +1,91 @@
+﻿namespace Rhythm.Drop.Web.Tests.TagHelperRenderers;
+
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+/// <summary>
+/// A base class for creating tests for Tag Helper Renderers.
+/// </summary>
+public abstract class TagHelperRendererTestsBase
+{
+    protected const string DefaultTagName = "div";
+
+    /// <summary>
+    /// Creates a <see cref="TagHelperContext"/> with a tag name and attributes.
+    /// </summary>
+    /// <param name="tagName">The tag name.</param>
+    /// <param name="attributes">The attributes</param>
+    /// <returns>A <see cref="TagHelperContext"/>.</returns>
+    protected static TagHelperContext CreateTagHelperContext(string tagName, IEnumerable<TagHelperAttribute> attributes)
+    {
+        return new TagHelperContext(tagName, new TagHelperAttributeList(attributes), new Dictionary<object, object>(), Guid.NewGuid().ToString());
+    }
+
+    /// <summary>
+    /// Creates a <see cref="TagHelperContext"/> with a tag name.
+    /// </summary>
+    /// <param name="tagName">The tag name.</param>
+    /// <param name="attributes">The attributes</param>
+    /// <returns>A <see cref="TagHelperContext"/>.</returns>
+    protected static TagHelperContext CreateTagHelperContext(string tagName)
+    {
+        return CreateTagHelperContext(tagName, []);
+    }
+
+    /// <summary>
+    /// Creates a <see cref="TagHelperOutput"/> with a tag name, attributes and HTML content.
+    /// </summary>
+    /// <param name="tagName">The tag name.</param>
+    /// <param name="attributes">The attributes</param>
+    /// <param name="htmlContent">The HTML content.</param>
+    /// <returns>A <see cref="TagHelperOutput"/>.</returns>
+    protected static TagHelperOutput CreateTagHelperOutput(string tagName, IEnumerable<TagHelperAttribute> attributes, IHtmlContent? htmlContent)
+    {
+        return new TagHelperOutput(tagName, new TagHelperAttributeList(attributes), (useCachedResult, encoder) =>
+        {
+            var tagHelperContent = new DefaultTagHelperContent();
+
+            if (htmlContent is not null)
+            {
+                tagHelperContent.SetHtmlContent(htmlContent);
+            }
+
+            return Task.FromResult<TagHelperContent>(tagHelperContent);
+        });
+    }
+
+    /// <summary>
+    /// Creates a <see cref="TagHelperOutput"/> with a tag name and HTML content.
+    /// </summary>
+    /// <param name="tagName">The tag name.</param>
+    /// <param name="htmlContent">The HTML content.</param>
+    /// <returns>A <see cref="TagHelperOutput"/>.</returns>
+    protected static TagHelperOutput CreateTagHelperOutput(string tagName, IHtmlContent? htmlContent)
+    {
+        return CreateTagHelperOutput(tagName, [], htmlContent);
+    }
+
+    /// <summary>
+    /// Creates a <see cref="TagHelperOutput"/> with a tag name and HTML content.
+    /// </summary>
+    /// <param name="tagName">The tag name.</param>
+    /// <param name="attributes">The attributes</param>
+    /// <returns>A <see cref="TagHelperOutput"/>.</returns>
+    protected static TagHelperOutput CreateTagHelperOutput(string tagName, IEnumerable<TagHelperAttribute> attributes)
+    {
+        return CreateTagHelperOutput(tagName, attributes, default);
+    }
+
+    /// <summary>
+    /// Creates a <see cref="TagHelperOutput"/> with a tag name and HTML content.
+    /// </summary>
+    /// <param name="tagName">The tag name.</param>
+    /// <returns>A <see cref="TagHelperOutput"/>.</returns>
+    protected static TagHelperOutput CreateTagHelperOutput(string tagName)
+    {
+        return CreateTagHelperOutput(tagName, [], default);
+    }
+}
