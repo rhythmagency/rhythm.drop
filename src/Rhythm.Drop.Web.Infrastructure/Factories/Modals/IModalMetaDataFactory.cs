79bf993321e89201cb815527bddf73ee005b5e59
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-02-05 16:55:10 -0500

    feat (Meta Data): creates initial Modal Meta Data factory and input

---
+++ src/Rhythm.Drop.Web.Infrastructure/Factories/Modals/IModalMetaDataFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace Rhythm.Drop.Web.Infrastructure.Factories.Modals;
+
+using Rhythm.Drop.Web.Infrastructure.MetaData.Modals;
+
+/// <summary>
+/// A factory for creating Modal Meta Data.
+/// </summary>
+public interface IModalMetaDataFactory : IMetaDataFactory<ModalMetaDataFactoryInput, ModalMetaData>
+{
+}
---
+++ src/Rhythm.Drop.Web.Infrastructure/Factories/Modals/ModalMetaDataFactoryInput.cs
@@ -0,0 +1,15 @@
+﻿namespace Rhythm.Drop.Web.Infrastructure.Factories.Modals;
+
+using Rhythm.Drop.Models.Common.Attributes;
+using Rhythm.Drop.Models.Modals;
+
+/// <summary>
+/// The input required by <see cref="IModalMetaDataFactory"/>.
+/// </summary>
+/// <param name="Modal">The modal.</param>
+/// <param name="Theme">The theme of the modal.</param>
+/// <param name="Attributes">The attributes to be passed to the modal.</param>
+/// <param name="Section">The optional section of where this modal is rendered.</param>
+public sealed record ModalMetaDataFactoryInput(IModal Modal, string Theme, IReadOnlyHtmlAttributeCollection Attributes, string? Section)
+{
+}
