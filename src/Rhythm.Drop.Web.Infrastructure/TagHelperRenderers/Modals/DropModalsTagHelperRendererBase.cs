423b63dc7d28665b2c8eb79a29db81066a7a08d9
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-02-05 17:20:38 -0500

    feat (THRs): creates DropModalsTagHelperRendererBase
    Right now this is a copy of DropComponentsTagHelperRendererBase. In a future updates there should be a look at replacing some of this logic with a base class to share logic.

---
+++ src/Rhythm.Drop.Web.Infrastructure/TagHelperRenderers/Modals/DropModalsTagHelperRendererBase.cs
@@ -0,0 +1,84 @@
+﻿namespace Rhythm.Drop.Web.Infrastructure.TagHelperRenderers.Modals;
+
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Rhythm.Drop.Models.Modals;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+/// <summary>
+/// A base class for rendering a collection of <see cref="IModal"/> when used in a <see cref="TagHelper"/>.
+/// </summary>
+public abstract class DropModalsTagHelperRendererBase : IDropModalsTagHelperRenderer
+{
+    /// <inheritdoc/>
+    public async Task RenderAsync(DropModalsTagHelperRendererContext? model, TagHelperContext context, TagHelperOutput output)
+    {
+        if (IsNullOrEmpty(model))
+        {
+            await RenderNullAsync(output);
+            return;
+        }
+
+        await RenderMultipleAsync(model, context, output);
+    }
+
+    /// <inheritdoc/>
+    public async Task RenderAsync(DropModalTagHelperRendererContext? model, TagHelperContext context, TagHelperOutput output)
+    {
+        if (IsNull(model))
+        {
+            await RenderNullAsync(output);
+            return;
+        }
+
+        await RenderSingleAsync(model, context, output);
+    }
+
+    /// <summary>
+    /// Renders multiple modals using a <see cref="DropModalsTagHelperRendererContext"/>.
+    /// </summary>
+    /// <param name="model">The model.</param>
+    /// <param name="context">The context.</param>
+    /// <param name="output">The output.</param>
+    /// <returns>A <see cref="Task"/>.</returns>
+    protected abstract Task RenderMultipleAsync(DropModalsTagHelperRendererContext model, TagHelperContext context, TagHelperOutput output);
+
+    /// <summary>
+    /// Renders a single modal using a <see cref="DropModalTagHelperRendererContext"/>.
+    /// </summary>
+    /// <param name="model">The model.</param>
+    /// <param name="context">The context.</param>
+    /// <param name="output">The output.</param>
+    /// <returns>A <see cref="Task"/>.</returns>
+    protected abstract Task RenderSingleAsync(DropModalTagHelperRendererContext model, TagHelperContext context, TagHelperOutput output);
+
+    /// <summary>
+    /// Checks if the model is null.
+    /// </summary>
+    /// <param name="model">The model.</param>
+    /// <returns>A <see cref="bool"/>.</returns>
+    protected virtual bool IsNullOrEmpty([NotNullWhen(false)] DropModalsTagHelperRendererContext? model)
+    {
+        return model is null || model.Modals.Count == 0;
+    }
+
+    /// <summary>
+    /// Checks if the model is null.
+    /// </summary>
+    /// <param name="model">The model.</param>
+    /// <returns>A <see cref="bool"/>.</returns>
+    protected virtual bool IsNull([NotNullWhen(false)] DropModalTagHelperRendererContext? model)
+    {
+        return model is null;
+    }
+
+    /// <summary>
+    /// Renders the output for a null scenario.
+    /// </summary>
+    /// <param name="output">The output.</param>
+    /// <returns>A <see cref="Task"/>.</returns>
+    protected virtual async Task RenderNullAsync(TagHelperOutput output)
+    {
+        await Task.Run(output.SuppressOutput);
+    }
+}
