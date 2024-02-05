622b755e1ec920ee305b9876b4923bee7cb01f38
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-02-05 16:56:31 -0500

    feat (Meta Data): adds SetModalMetaDataFactory extension

--- src/Rhythm.Drop.Web.Infrastructure/Factories/RhythmDropBuilderExtensions.cs
+++ src/Rhythm.Drop.Web.Infrastructure/Factories/RhythmDropBuilderExtensions.cs
@@ -3,6 +3,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Rhythm.Drop.Infrastructure;
 using Rhythm.Drop.Web.Infrastructure.Factories.Components;
+using Rhythm.Drop.Web.Infrastructure.Factories.Modals;
 
 /// <summary>
 /// A collection of extension methods for <see cref="IRhythmDropBuilder"/> related to factories.
@@ -18,6 +19,18 @@ public static class RhythmDropBuilderExtensions
     public static IRhythmDropBuilder SetComponentMetaDataFactory<TFactory>(this IRhythmDropBuilder builder) where TFactory : class, IComponentMetaDataFactory
     {
         builder.Services.Replace<IComponentMetaDataFactory, TFactory>(ServiceLifetime.Singleton);
+        return builder;
+    }
+
+    /// <summary>
+    /// Sets the <see cref="IModalMetaDataFactory"/>.
+    /// </summary>
+    /// <typeparam name="TFactory">The type of the new factory.</typeparam>
+    /// <param name="builder">The current <see cref="IRhythmDropBuilder"/>.</param>
+    /// <remarks>Returns the current <see cref="IRhythmDropBuilder"/>.</remarks>
+    public static IRhythmDropBuilder SetModalMetaDataFactory<TFactory>(this IRhythmDropBuilder builder) where TFactory : class, IModalMetaDataFactory
+    {
+        builder.Services.Replace<IModalMetaDataFactory, TFactory>(ServiceLifetime.Singleton);
         return builder;
     }
 }
