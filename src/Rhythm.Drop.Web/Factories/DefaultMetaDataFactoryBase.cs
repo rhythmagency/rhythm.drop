2ce5169c23460977fe6f3fde2a3110d1e1bffff3
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-02-05 17:05:35 -0500

    feat (Meta Data): creates Default Meta Data factory base
    This will share as much logic between the different meta data factories as possible.

---
+++ src/Rhythm.Drop.Web/Factories/DefaultMetaDataFactoryBase.cs
@@ -0,0 +1,26 @@
+﻿namespace Rhythm.Drop.Web.Factories;
+
+using Rhythm.Drop.Web.Infrastructure.Factories;
+using Rhythm.Drop.Web.Infrastructure.MetaData;
+using System;
+
+internal abstract class DefaultMetaDataFactoryBase<TInput, TMetaData> : IMetaDataFactory<TInput, TMetaData> where TMetaData : MetaData
+{
+    public TMetaData Create(TInput input)
+    {
+        var instanceType = CreateInstanceType(input);
+        var instanceParameters = CreateInstanceParameters(input);
+        var instance = Activator.CreateInstance(instanceType, instanceParameters);
+
+        if (instance is not TMetaData metaData)
+        {
+            throw new InvalidOperationException($"Unable to create meta data of type {typeof(TMetaData)}");
+        }
+
+        return metaData;
+    }
+
+    protected abstract object?[]? CreateInstanceParameters(TInput input);
+
+    protected abstract Type CreateInstanceType(TInput input);
+}
