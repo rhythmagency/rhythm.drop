99c2d7a1e5a62bc379ca2f6682c2128e804321b7
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-02-05 13:36:46 -0500

    feat (MetaData): adds base meta data

---
+++ src/Rhythm.Drop.Web.Infrastructure/MetaData/MetaData.cs
@@ -0,0 +1,8 @@
+﻿namespace Rhythm.Drop.Web.Infrastructure.MetaData;
+
+/// <summary>
+/// An abstract object for creating meta data.
+/// </summary>
+public abstract record MetaData(string Theme)
+{
+}
