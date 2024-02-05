58baf001641eae08b70b4013b75054f8f8cb9ca0
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-02-05 17:06:27 -0500

    feat (Meta Data): reworks Default Component Meta Data Factory
    This now uses Default Meta Factory Base under the hood to do most of the work.

--- src/Rhythm.Drop.Web/Factories/Components/DefaultComponentMetaDataFactory.cs
+++ src/Rhythm.Drop.Web/Factories/Components/DefaultComponentMetaDataFactory.cs
@@ -8,20 +8,25 @@ using System;
 /// <summary>
 /// The default implementation of <see cref="IComponentMetaDataFactory"/>.
 /// </summary>
-internal sealed class DefaultComponentMetaDataFactory : IComponentMetaDataFactory
+internal sealed class DefaultComponentMetaDataFactory : DefaultMetaDataFactoryBase<ComponentMetaDataFactoryInput, ComponentMetaData>, IComponentMetaDataFactory
 {
-    /// <inheritdoc/>
-    public ComponentMetaData Create(ComponentMetaDataFactoryInput input)
+    protected override object?[]? CreateInstanceParameters(ComponentMetaDataFactoryInput input)
+    {
+        return [
+            input.Component,
+            input.Level,
+            input.Index,
+            input.Total,
+            input.Theme,
+            input.Attributes,
+            input.Section,
+        ];
+    }
+
+    protected override Type CreateInstanceType(ComponentMetaDataFactoryInput input)
     {
         Type componentType = input.Component.GetType();
-        Type genericType = typeof(ComponentMetaData<>).MakeGenericType(componentType);
-        var instance = Activator.CreateInstance(genericType, [input.Component, input.Level, input.Index, input.Total, input.Theme, input.Attributes, input.Section]);
 
-        if (instance is not ComponentMetaData metaData)
-        {
-            throw new InvalidOperationException("Unable to create component meta data");
-        }
-
-        return metaData;
+        return typeof(ComponentMetaData<>).MakeGenericType(componentType);
     }
 }
