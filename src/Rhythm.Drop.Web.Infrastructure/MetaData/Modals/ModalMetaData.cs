6767d3e64327268000b5167185061874dac1f9f7
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-02-05 16:44:25 -0500

    feat (Meta Data): adds Modal Meta Data models

---
+++ src/Rhythm.Drop.Web.Infrastructure/MetaData/Modals/ModalMetaData.cs
@@ -0,0 +1,35 @@
+﻿namespace Rhythm.Drop.Web.Infrastructure.MetaData.Modals;
+
+using Rhythm.Drop.Models.Common.Attributes;
+using Rhythm.Drop.Models.Modals;
+
+/// <summary>
+/// An abstract non-generic type for Modal Meta Data.
+/// </summary>
+/// <param name="Theme">The theme of the modal.</param>
+/// <param name="Attributes">The additional HTML attributes.</param>
+/// <param name="Section">The optional section of where this modal is rendered.</param>
+/// <remarks>
+/// <para>
+/// This type exists to make it easier to construct and return a generic Modal Meta Data.
+/// </para>
+/// <para>
+/// It should not be used to create other types.
+/// </para>
+/// </remarks>
+public abstract record ModalMetaData(string Theme, IReadOnlyHtmlAttributeCollection Attributes, string? Section) : MetaData(Theme)
+{
+    /// <summary>
+    /// An abstract non-generic type for Modal Meta Data.
+    /// </summary>
+    /// <remarks>This creates a modal meta data without a section for backward compatability.</remarks>
+    public ModalMetaData(string Theme, IReadOnlyHtmlAttributeCollection Attributes) : this(Theme, Attributes, default)
+    {
+    }
+
+    /// <summary>
+    /// Gets the modal of the meta data.
+    /// </summary>
+    /// <returns>A <see cref="IModal"/>.</returns>
+    public abstract IModal GetModal();
+}
---
+++ src/Rhythm.Drop.Web/MetaData/Modals/ModalMetaDataOfTModal.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Rhythm.Drop.Web.MetaData.Modals;
+
+using Rhythm.Drop.Models.Common.Attributes;
+using Rhythm.Drop.Models.Modals;
+using Rhythm.Drop.Web.Infrastructure.MetaData.Modals;
+
+/// <summary>
+/// A generic type for Modal Meta Data.
+/// </summary>
+/// <typeparam name="TModal">The type of the modal.</typeparam>
+/// <param name="Modal">The modal.</param>
+/// <param name="Theme">The theme of the modal.</param>
+/// <param name="Attributes">The attributes to be passed to the modal.</param>
+/// <param name="Section">The optional section of where this modal is rendered.</param>
+public sealed record ModalMetaData<TModal>(TModal Modal, string Theme, IReadOnlyHtmlAttributeCollection Attributes, string? Section) : ModalMetaData(Theme, Attributes, Section) where TModal : IModal
+{
+    /// <summary>
+    /// A generic type for Modal Meta Data without a section.
+    /// </summary>
+    public ModalMetaData(TModal Modal, string Theme, IReadOnlyHtmlAttributeCollection Attributes) : this(Modal, Theme, Attributes, default)
+    {
+    }
+
+    /// <inheritdoc/>
+    public override IModal GetModal()
+    {
+        return Modal;
+    }
+
+    /// <summary>
+    /// Gets all the attributes from the meta data and the modal.
+    /// </summary>
+    /// <returns>A <see cref="IReadOnlyHtmlAttributeCollection"/>.</returns>
+    public IReadOnlyHtmlAttributeCollection AllAttributes()
+    {
+        var collection = new HtmlAttributeCollection(Attributes);
+        collection.SetAttributes(Modal.Attributes);
+
+        return collection.ToReadOnly();
+    }
+}
