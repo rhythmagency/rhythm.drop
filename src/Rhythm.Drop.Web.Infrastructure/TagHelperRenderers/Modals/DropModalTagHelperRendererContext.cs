bae1b3fc65c1a347e49bc33a845aff12cf4da3f6
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-02-05 17:18:13 -0500

    feat (THRs): adds initial work for modal THRs

---
+++ src/Rhythm.Drop.Web.Infrastructure/TagHelperRenderers/Modals/DropModalTagHelperRendererContext.cs
@@ -0,0 +1,17 @@
+﻿namespace Rhythm.Drop.Web.Infrastructure.TagHelperRenderers.Modals;
+
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Rhythm.Drop.Models.Common.Attributes;
+using Rhythm.Drop.Models.Modals;
+
+/// <summary>
+/// The context required to render a <see cref="IDropModalsTagHelperRenderer"/>
+/// </summary>
+/// <param name="Modal">The modal.</param>
+/// <param name="Theme">The theme.</param>
+/// <param name="Attributes">The attributes.</param>
+/// <param name="ViewContext">The view context</param>
+/// <param name="Section">The optional section of where this modal is rendered.</param>
+public sealed record DropModalTagHelperRendererContext(IModal? Modal, string Theme, IReadOnlyHtmlAttributeCollection Attributes, ViewContext ViewContext, string? Section)
+{
+}
---
+++ src/Rhythm.Drop.Web.Infrastructure/TagHelperRenderers/Modals/DropModalsTagHelperRendererContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Rhythm.Drop.Web.Infrastructure.TagHelperRenderers.Modals;
+
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Rhythm.Drop.Models.Modals;
+
+/// <summary>
+/// The context required to render a <see cref="IDropModalsTagHelperRenderer"/>
+/// </summary>
+/// <param name="Modals">The modals.</param>
+/// <param name="Theme">The theme.</param>
+/// <param name="TagName">The optional tag name.</param>
+/// <param name="ViewContext">The view context</param>
+/// <param name="Section">The optional section of where this modal is rendered.</param>
+public sealed record DropModalsTagHelperRendererContext(IReadOnlyCollection<IModal> Modals, string Theme, string? TagName, ViewContext ViewContext, string? Section)
+{
+}
