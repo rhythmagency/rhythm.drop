175aec0cf18266afe3ba0c21ba9f62d90b0b6173
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-02-05 16:50:13 -0500

    feat (Meta Data): adds generic IMetaDataFactory

---
+++ src/Rhythm.Drop.Web.Infrastructure/Factories/IMetaDataFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace Rhythm.Drop.Web.Infrastructure.Factories;
+
+using Rhythm.Drop.Web.Infrastructure.MetaData;
+
+/// <summary>
+/// A contract for creating a factory that creates <see cref="MetaData"/>.
+/// </summary>
+public interface IMetaDataFactory<in TInput, out TMetaData> where TMetaData : MetaData
+{
+    /// <summary>
+    /// Creates a <typeparamref name="TMetaData"/> for a given input.
+    /// </summary>
+    /// <param name="input">The input required to create a <typeparamref name="TMetaData"/> object.</param>
+    /// <returns>A <typeparamref name="TMetaData"/>.</returns>
+    TMetaData Create(TInput input);
+}
