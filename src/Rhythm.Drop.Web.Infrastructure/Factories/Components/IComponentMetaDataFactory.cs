9bf748ac5f3149ab2b5f5c4a4e6238cd0532ca72
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-02-05 16:50:32 -0500

    feat (Meta Data): updates IComponentMetaDataFactory to use IMetaDataFactory

--- src/Rhythm.Drop.Web.Infrastructure/Factories/Components/IComponentMetaDataFactory.cs
+++ src/Rhythm.Drop.Web.Infrastructure/Factories/Components/IComponentMetaDataFactory.cs
@@ -5,12 +5,6 @@ using Rhythm.Drop.Web.Infrastructure.MetaData.Components;
 /// <summary>
 /// A factory for creating Component Meta Data.
 /// </summary>
-public interface IComponentMetaDataFactory
+public interface IComponentMetaDataFactory : IMetaDataFactory<ComponentMetaDataFactoryInput, ComponentMetaData>
 {
-    /// <summary>
-    /// Creates a <see cref="ComponentMetaData"/> for a given input.
-    /// </summary>
-    /// <param name="input">The input required to create a <see cref="ComponentMetaData"/> object.</param>
-    /// <returns>A <see cref="ComponentMetaData"/>.</returns>
-    ComponentMetaData Create(ComponentMetaDataFactoryInput input);
 }
