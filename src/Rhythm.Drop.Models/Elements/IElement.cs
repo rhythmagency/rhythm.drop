47e825785846f219a29db0b2d3afd24752762b8e
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-02-06 10:50:59 -0500

    feat (Elements): adds initial interface

---
+++ src/Rhythm.Drop.Models/Elements/IElement.cs
@@ -0,0 +1,11 @@
+﻿namespace Rhythm.Drop.Models.Elements;
+
+using Rhythm.Drop.Models.Common;
+
+/// <summary>
+/// A contract all elements must implement.
+/// </summary>
+/// <remarks>Elements are a renderable item that belongs to a component. It should be self-contained but it is not standalone.</remarks>
+public interface IElement : IHaveViewName
+{
+}
