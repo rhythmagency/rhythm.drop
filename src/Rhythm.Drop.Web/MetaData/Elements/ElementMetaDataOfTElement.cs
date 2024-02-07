060de81f03b05b4da9cb7dc48d113461a044b63f
Jamie Pollock <jamie.pollock@rhythmagency.com>
2024-02-07 12:19:49 -0500

    feat (Meta Data): implements generic Element Meta Data and extensions

---
+++ src/Rhythm.Drop.Web/MetaData/Elements/ElementMetaDataOfTElement.cs
@@ -0,0 +1,32 @@
+﻿namespace Rhythm.Drop.Web.MetaData.Elements;
+
+using Rhythm.Drop.Models.Common.Attributes;
+using Rhythm.Drop.Models.Elements;
+using Rhythm.Drop.Web.Infrastructure.MetaData.Elements;
+
+/// <summary>
+/// A generic type for Element Meta Data.
+/// </summary>
+/// <typeparam name="TElement">The type of the element.</typeparam>
+/// <param name="Element">The element.</param>
+/// <param name="Index">The index of the element within the current collection of elements.</param>
+/// <param name="Total">The total number of elements within the current collection of elements.</param>
+/// <param name="Theme">The theme of the element.</param>
+/// <param name="Attributes">The attributes to be passed to the element.</param>
+/// <param name="Section">The optional section of where this element is rendered.</param>
+public sealed record ElementMetaData<TElement>(TElement Element, int Index, int Total, string Theme, IReadOnlyHtmlAttributeCollection Attributes, string? Section) : ElementMetaData(Index, Total, Theme, Attributes, Section) where TElement : IElement
+{
+    /// <summary>
+    /// A generic type for Element Meta Data
+    /// </summary>
+    /// <remarks>This creates a element meta data without a section for backward compatability.</remarks>
+    public ElementMetaData(TElement Element, int Index, int Total, string Theme, IReadOnlyHtmlAttributeCollection Attributes) : this(Element, Index, Total, Theme, Attributes, default)
+    {
+    }
+
+    /// <inheritdoc/>
+    public override IElement GetElement()
+    {
+        return Element;
+    }
+}
---
+++ src/Rhythm.Drop.Web/MetaData/Elements/ElementMetaDataOfTElementExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Rhythm.Drop.Web.MetaData.Elements;
+
+using Rhythm.Drop.Models.Common;
+using Rhythm.Drop.Models.Common.Attributes;
+using Rhythm.Drop.Models.Elements;
+
+/// <summary>
+/// A collection of extension methods that augment <see cref="ElementMetaData{TElement}"/>.
+/// </summary>
+public static class ElementMetaDataOfTypeExtensions
+{
+    /// <summary>
+    /// Gets all the attributes from the meta data and the element.
+    /// </summary>
+    /// <typeparam name="TElement">The type of the element.</typeparam>
+    /// <param name="metaData">The current element meta data.</param>
+    /// <returns>A <see cref="IReadOnlyHtmlAttributeCollection"/>.</returns>
+    public static IReadOnlyHtmlAttributeCollection AllAttributes<TElement>(this ElementMetaData<TElement> metaData) where TElement : class, IElement, IHaveAttributes
+    {
+        var collection = new HtmlAttributeCollection(metaData.Attributes);
+        collection.SetAttributes(metaData.Attributes);
+
+        return collection.ToReadOnly();
+    }
+}
